af74d65808fcdfe9d75e7eb3d62f1c9e976eaa62
Taras Ostasha <tonyjoss1990@gmail.com>
2026-01-11 18:29:38 -0500

    done

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        'code': error['code'],
+        'line': error['line_number'],
+        'column': error['column_number'],
+        'text': error['text'],
+    }
+
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        'file': file_path,
+        'errors': [format_linter_error(error) for error in errors],
+    }
+
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+        if errors
+    ]
+
